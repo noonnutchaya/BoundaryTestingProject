ed97a0ae14ff8ac9df8c85e1d783ea852ab7964a
noonnutchaya <chocola.2542@hotmail.com>
2021-01-13 18:19:35 +0700

    update code

--- BoundaryTesting.py
+++ BoundaryTesting.py
@@ -6,12 +6,23 @@ y_min = ''
 y_max = ''
 type_ = ''
 
-while x_min is not int:
-    try:
-        x_min = float(input('Enter x min: '))
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+while True:
+    while x_min is not int:
+        try:
+            x_min = float(input('Enter x min: '))
+            break
+        except ValueError:
+            print('Please enter a valid number ')
+    
+    if x_min < 0:
+        print('Please enter a positive number ')
+    elif x_min >= 5000:
+        print("x min should less than maximun number (Maximun number: 5000)")
+    else:
         break
-    except ValueError:
-        print('Please enter a valid number: ')
 
 while True:
     while x_max is not int:
@@ -21,20 +32,32 @@ while True:
         except ValueError:
             print('Please enter a valid number: ')
 
-    if x_max <= x_min:
+    if x_max <= x_min and x_max >= 0:
         print('Please input correct value')
+    elif x_max > 5000 :
+        print("Please input correct value (Maximun number: 5000)")
+    elif x_max < 0:
+        print('Please enter a positive number')
     elif x_max > x_min:
         break
 print('----------------------')
 
 # ---------------------------------------------------
 
-while y_min is not int:
-    try:
-        y_min = float(input('Enter y min: '))
+while True:
+    while y_min is not int:
+        try:
+            y_min = float(input('Enter y min: '))
+            break
+        except ValueError:
+            print('Please enter a valid number')
+    
+    if y_min < 0:
+        print('Please enter a positive number')
+    elif y_min >= 5000:
+        print("y min should less than maximun number")
+    else:
         break
-    except ValueError:
-        print('Please enter a valid number: ')
 
 while True:
     while y_max is not int:
@@ -42,10 +65,12 @@ while True:
             y_max = float(input('Enter y max: '))
             break
         except ValueError:
-            print('Please enter a valid number: ')
+            print('Please enter a valid number')
 
     if y_max <= y_min:
         print('Please input correct value')
+    elif y_max > 5000 :
+        print("Please input correct value (Maximun number: 5000)")
     elif y_max > y_min:
         break
 
@@ -55,8 +80,7 @@ print('[1] BVA')
 print('[2] Worst Case')
 print('[3] Robustness')
 print('[4] Worst Case Robustness')
-x_check = x_max - x_min
-y_check = y_max - y_min
+
 x_diff =  ((x_max - x_min)/100*10)
 y_diff = ((y_max - y_min)/100*10)
 
@@ -72,18 +96,21 @@ while True:
         x_mid = (x_min+x_max)/2
         list_x = [ x_min, x_min+x_diff, x_mid, x_mid, x_mid , x_mid , x_mid , x_max-x_diff, x_max]
         list_y = [ y_mid, y_mid, y_mid, y_max-y_diff, y_max,y_min+y_diff, y_min , y_mid, y_mid]
+        plt.suptitle('BVA')
         break
     if type_ == 2 :
         y_mid = (y_max+y_min)/2
         x_mid = (x_min+x_max)/2
         list_x = [ x_min, x_min+x_diff, x_mid, x_mid, x_mid , x_mid , x_mid , x_max-x_diff, x_max, x_min, x_min, x_min,x_min, x_min+x_diff, x_min+x_diff, x_min+x_diff, x_min+x_diff, x_max-x_diff, x_max-x_diff, x_max-x_diff, x_max-x_diff,x_max, x_max,x_max,x_max]
         list_y = [ y_mid, y_mid, y_mid, y_max-y_diff, y_max,y_min+y_diff, y_min , y_mid, y_mid, y_max,y_max-y_diff, y_min+y_diff,y_min,y_max, y_max-y_diff, y_min+y_diff,y_min,y_max, y_max-y_diff, y_min+y_diff, y_min,y_max,y_max-y_diff,y_min+y_diff,y_min]
+        plt.suptitle('Worst Case')
         break
     if type_ == 3 :
         y_mid = (y_max+y_min)/2
         x_mid = (x_min+x_max)/2
         list_x = [ x_min, x_min+x_diff, x_mid, x_mid    , x_mid     , x_mid   , x_mid , x_max-x_diff, x_max   , x_min-x_diff , x_mid ,x_mid , x_max+x_diff]
         list_y = [ y_mid, y_mid    , y_mid, y_max-y_diff, y_max ,y_min+y_diff, y_min , y_mid    , y_mid    ,y_mid, y_max+y_diff , y_min-y_diff, y_mid]
+        plt.suptitle('Robustness')
         break
     
     if type_ == 4 :
@@ -91,10 +118,18 @@ while True:
         x_mid = (x_min+x_max)/2
         list_x = [ x_min, x_min+x_diff, x_mid, x_mid, x_mid , x_mid , x_mid , x_max-x_diff, x_max, x_min, x_min, x_min,x_min, x_min+x_diff, x_min+x_diff, x_min+x_diff, x_min+x_diff, x_max-x_diff, x_max-x_diff, x_max-x_diff, x_max-x_diff,x_max, x_max,x_max,x_max, x_min-x_diff, x_min-x_diff, x_min-x_diff, x_min-x_diff, x_min-x_diff, x_min-x_diff, x_min-x_diff, x_min, x_min, x_min+x_diff, x_min+x_diff, x_mid ,x_mid ,x_max-x_diff, x_max-x_diff, x_max ,x_max, x_max+x_diff , x_max+x_diff, x_max+x_diff, x_max+x_diff, x_max+x_diff, x_max+x_diff, x_max+x_diff]
         list_y = [ y_mid, y_mid, y_mid, y_max-y_diff, y_max,y_min+y_diff, y_min , y_mid, y_mid, y_max,y_max-y_diff, y_min+y_diff,y_min,y_max, y_max-y_diff, y_min+y_diff,y_min,y_max, y_max-y_diff, y_min+y_diff, y_min,y_max,y_max-y_diff,y_min+y_diff,y_min,y_max+y_diff, y_max, y_max-y_diff , y_mid, y_min+y_diff , y_min, y_min-y_diff, y_max+y_diff, y_min-y_diff, y_max+y_diff, y_min-y_diff,y_max+y_diff, y_min-y_diff, y_max+y_diff, y_min-y_diff, y_max+y_diff, y_min-y_diff,y_max+y_diff,y_max,y_max-y_diff, y_mid, y_min+y_diff, y_min, y_min-y_diff]
+        plt.suptitle('Worst Case Robustness')
         break
     else: 
         print('Please select correct type')
+
 plt.scatter(list_x,list_y)
 plt.ylabel('Y value')
 plt.xlabel('X value')
+
+for i,j in zip(list_x,list_y):
+    ax.annotate('%s)' %j, xy=(i,j), xytext=(30,0), textcoords='offset points')
+    ax.annotate('(%s,' %i, xy=(i,j))
+
+plt.grid()
 plt.show()
